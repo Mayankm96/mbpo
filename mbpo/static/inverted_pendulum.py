0948009afe3522511a2c574a66c5c90c2a6b8409
Mayank Mittal <mayankm.iitk@gmail.com>
2020-06-02 10:51:00 +0200

    corrects termination function for simbl-inverted pendulum

--- mbpo/static/inverted_pendulum.py
+++ mbpo/static/inverted_pendulum.py
@@ -2,16 +2,35 @@ import sys
 import numpy as np
 import pdb
 
+"""
+Fo MuJoCo Inverted Pendulum
+"""
+# class StaticFns:
+#
+#     @staticmethod
+#     def termination_fn(obs, act, next_obs):
+#         assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
+#
+#         notdone = np.isfinite(next_obs).all(axis=-1) \
+#         		  * (np.abs(next_obs[:,1]) <= .2)
+#         done = ~notdone
+#
+#         done = done[:,None]
+#
+#         return done
+
+"""
+For SiMBL Inverted Pendulum
+"""
 class StaticFns:
 
     @staticmethod
     def termination_fn(obs, act, next_obs):
         assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-        notdone = np.isfinite(next_obs).all(axis=-1) \
-        		  * (np.abs(next_obs[:,1]) <= .2)
-        done = ~notdone
+        state_high = np.array([1, 1])
+        state_term = np.any(next_obs > state_high) or np.any(next_obs < -state_high)
 
-        done = done[:,None]
+        done = state_term
 
         return done
